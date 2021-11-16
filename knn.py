6144372bc3d176ecd468987f42006a41dd47602e
alina-tariq <alina.tariq@ryerson.ca>
2021-11-16 16:21:07 -0500

    added k-nearest neighbours algo

--- knn.py
+++ knn.py
@@ -1,32 +1,36 @@
-from utils import *
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RepeatedKFold
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
 import random
+import numpy as np
 
-data = read_csv_file("heart_processed.csv", remove_header = False, convert_to_float = True)
+def KNN_Clasifier(x, y, neighbours, fold, repeat):
+    """
+    Implements the k-Nearest Neighbours classifier
+    :param x: input
+    :param y: output
+    :param neighbours: number of neighbours
+    :param fold: number of folds to split dataset into
+    :param repeat: number of iterations
+    :return: average accuracy score for given params
+    """
 
-X = data[:, :-1]
-X = add_intercept(X)
-Y = data[:, -1]
+    x_norm = MinMaxScaler().fit_transform(x)
+    scores = []
 
-splits = {0.15: "85-15", 0.20: "80-20", 0.25: "75-25", 0.30: "70-30", 0.35: "65:35"}
+    for neighbour in neighbours:
+        clf = KNeighborsClassifier(n_neighbors=neighbour)
+        rskf = RepeatedKFold(n_splits=fold, n_repeats=repeat, random_state=random.randint(1, 1000000))
+        score = np.zeros((fold * repeat,))
+        i = 0
 
-for split in splits.keys():
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=split, random_state=random.randint(1, 10000))
-    acc = 0
-    clf = KNeighborsClassifier()
+        for train_index, test_index in rskf.split(x_norm, y):
+            x_train, x_test = x_norm[train_index], x_norm[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            clf.fit(x_train, y_train)
+            score[i] = clf.score(x_test, y_test)
+            i += 1
 
-    # fits, predicts, and scores the KNN model for 1000 iterations
-    # note: only last set of predictions are accessible but that can be changed
-    # also note: default params for the KNN model were used and we might want to change that
-    # last note: we should experiment with the sklearn.cross_validation particularly the
-    # cross_val_score function with shuffle=True to figure out optimal k value
-    
-    for i in range(1000):
-        clf.fit(x_train, y_test)
-        pred = clf.predict(x_test)  # these are predictions
-        acc += clf.score(x_test, y_test)
+        scores.append(score.mean())
 
-    mean_acc = acc/1000 * 100
-
-    print("The mean accuracy of the KNN model with a {} split is {:.2f}%".format(splits[split], mean_acc))
+    return np.array(scores)
