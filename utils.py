68b1016c8eb471baed960e6df669edb773168e92
alina-tariq <alina.tariq@ryerson.ca>
2021-11-16 16:23:03 -0500

    added load and split dataset functions

--- utils.py
+++ utils.py
@@ -1,5 +1,41 @@
 import csv
+import random
 import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def load_dataset(file, remove_header=False, convert_to_float=True):
+    """
+    Imports the dataset from the given file
+    :param file: file containing dataset
+    :param remove_header: removes file header (default=False)
+    :param convert_to_float: converts numbers to float (default=True)
+    :return: X (input) and Y (output) of data
+    """
+    data = read_csv_file(file, remove_header=remove_header, convert_to_float=convert_to_float)
+
+    x = data[:, :-1]  # get all columns except last
+    x = add_intercept(x)  # add 1's to first column
+    y = data[:, -1]  # get last column
+
+    return x, y
+
+
+def split_data(x, y, split_size, remove_header=False, convert_to_float=True):
+    """
+    Splits the dataset into training and test sets using the given ratio
+    :param x: dataset input
+    :param y: dataset output
+    :param split_size: ratio of test to training set split
+    :param remove_header: removes file header (default=False)
+    :param convert_to_float: converts numbers to float (default=True)
+    :return: training and test set
+    """
+    x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                        test_size=split_size, random_state=random.randint(1, 10000))
+
+    return x_train, x_test, y_train, y_test
+
 
 def add_intercept(x):
     """Add intercept to matrix x.
@@ -13,6 +49,7 @@ def add_intercept(x):
     new_x[:, 1:] = x
     return new_x
 
+
 def read_csv_file(filename, remove_header=True, convert_to_float=False):
     """Reads csv file and returns a numpy array"""
     with open(filename, newline='') as f:
@@ -23,6 +60,7 @@ def read_csv_file(filename, remove_header=True, convert_to_float=False):
         if remove_header:
             next(reader, None)
         return np.array(list(reader))
+
 
 def save_to_file(nparray, filename):
     """Writes nparray to filename"""
