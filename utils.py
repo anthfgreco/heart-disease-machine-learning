73c1d3b9bbd5eaf4d7ee80282959a242667e9dd7
alina-tariq <alina.tariq@ryerson.ca>
2021-12-07 19:39:25 -0500

    updated utils functions

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
-import csv
-import random
-import numpy as np
 from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import random
 
 
-def load_dataset(file, remove_header=False, convert_to_float=True):
+def load_dataset(file, header=None):
     """
     Imports the dataset from the given file
     :param file: file containing dataset
@@ -12,11 +12,10 @@ def load_dataset(file, remove_header=False, convert_to_float=True):
     :param convert_to_float: converts numbers to float (default=True)
     :return: X (input) and Y (output) of data
     """
-    data = read_csv_file(file, remove_header=remove_header, convert_to_float=convert_to_float)
+    dataset = np.array(pd.read_csv(file, header=header))
 
-    x = data[:, :-1]  # get all columns except last
-    x = add_intercept(x)  # add 1's to first column
-    y = data[:, -1]  # get last column
+    x = dataset[:, :-1]  # get all columns except last
+    y = dataset[:, -1]  # get last column
 
     return x, y
 
@@ -35,33 +34,3 @@ def split_data(x, y, split_size, remove_header=False, convert_to_float=True):
                                                         test_size=split_size, random_state=random.randint(1, 10000))
 
     return x_train, x_test, y_train, y_test
-
-
-def add_intercept(x):
-    """Add intercept to matrix x.
-    Args:
-        x: 2D NumPy array.
-    Returns:
-        New matrix same as x with 1's in the 0th column.
-    """
-    new_x = np.zeros((x.shape[0], x.shape[1] + 1), dtype=x.dtype)
-    new_x[:, 0] = 1
-    new_x[:, 1:] = x
-    return new_x
-
-
-def read_csv_file(filename, remove_header=True, convert_to_float=False):
-    """Reads csv file and returns a numpy array"""
-    with open(filename, newline='') as f:
-        if convert_to_float:
-            reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-        else:
-            reader = csv.reader(f)
-        if remove_header:
-            next(reader, None)
-        return np.array(list(reader))
-
-
-def save_to_file(nparray, filename):
-    """Writes nparray to filename"""
-    np.savetxt(filename, nparray, '%.1f', delimiter=",")
